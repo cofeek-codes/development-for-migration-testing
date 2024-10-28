c29e531503ca4fb46d3da0f65bb6f9de4c67d0f8
cofeek-codes <kormyshev11@mail.ru>
2024-10-28 14:26:04 +0500

    margins fixed

--- client/components/auth/RegisterForm.tsx
+++ client/components/auth/RegisterForm.tsx
@@ -34,13 +34,13 @@ const RegisterForm = (props: Props) => {
 						<label className='text-3xl ml-2.5' htmlFor='password__input'>
 							Пароль
 						</label>
+						{/* @FIXME: fix password focus area */}
 						<div className='c-password h-[90px] w-full text-3xl px-[30px] py-[26px] mt-[10px] mb-[20px] rounded-[10px] text-black bg-white'>
 							<input
 								type={!isPasswordShown ? 'password' : 'text'}
 								className='w-full h-full bg-inherit'
 							/>
 							<div
-								// @FIXME: fix eye_shown icon
 								className={
 									!isPasswordShown ? 'c-password-eye' : 'c-password-eye_shown'
 								}
@@ -50,7 +50,7 @@ const RegisterForm = (props: Props) => {
 						<label className='text-3xl ml-2.5' htmlFor='password__input'>
 							Кто ты?
 						</label>
-						<div className='c-select flex justify-center items-center h-[90px] w-full text-3xl px-[30px] py-[26px] mt-[10px] mb-[20px] rounded-[10px] text-black bg-white appearance-none'>
+						<div className='c-select flex justify-center items-center h-[90px] w-full text-3xl px-[30px] py-[26px] mt-[10px] rounded-[10px] text-black bg-white appearance-none'>
 							<select className='appearance-none w-full h-full bg-inherit'>
 								<option value='student'>Студент</option>
 								<option value='teacher'>Преподаватель</option>
@@ -77,13 +77,13 @@ const RegisterForm = (props: Props) => {
 						</label>
 						<input
 							type='text'
-							className='h-[90px] w-full text-3xl px-[30px] py-[26px] mt-[10px] rounded-[10px] mb-[20px] text-black bg-white'
+							className='h-[90px] w-full text-3xl px-[30px] py-[26px] mt-[10px] rounded-[10px] text-black bg-white'
 						/>
 					</div>
 				</div>
 				<div className='text-right mx-0 my-[30px]'>
 					<button className='text-[25px]' onClick={() => props.changeForm()}>
-						Вход
+						Авторизация
 					</button>
 				</div>
 				<button className='h-20 w-full text-3xl bg-lightPurple rounded-[10px] transition-[0.5s] hover:transition-[0.5s] hover:brightness-[90%] active:brightness-[70%]'>
