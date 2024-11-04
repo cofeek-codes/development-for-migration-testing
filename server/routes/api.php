5411190710f5e7c71abc2bdd0f45daef578d4a99
Renakezo <tchernosckutoff.egor@yandex.ru>
2024-11-04 23:08:46 +0500

    modified api.php and add AdminController

--- server/routes/api.php
+++ server/routes/api.php
@@ -1,17 +1,10 @@
 <?php
 
-use App\Http\Controllers\AnswerController;
+use App\Http\Controllers\AdminController;
 use App\Http\Controllers\AuthController;
-use App\Http\Controllers\GroupController;
-use App\Http\Controllers\LectureController;
-use App\Http\Controllers\MarkController;
 use App\Http\Controllers\ProjectController;
-use App\Http\Controllers\QuestionController;
-use App\Http\Controllers\SubjectController;
-use App\Http\Controllers\TestController;
 use App\Http\Controllers\TopicController;
 use App\Http\Controllers\UserController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -31,11 +24,13 @@ Route::controller(AuthController::class)->prefix('/auth')->group(function () {
 	// For tests
 	Route::get('/add', 'add');
 });
-Route::controller(UserController::class)->group(function () {});
-Route::controller(AnswerController::class)->group(function () {});
-Route::controller(GroupController::class)->group(function () {});
-Route::controller(LectureController::class)->group(function () {});
-Route::controller(MarkController::class)->group(function () {});
+Route::controller(UserController::class)->prefix('/user')->group(function () {
+	Route::get('/getSubjects', 'getSubjects');
+	Route::get('/getUser', 'getUser');
+	Route::get('/getGroups/{subject_id}', 'getGroups');
+	Route::get('/getTopics/{subject_id}/{group_id}', 'getToipcs');
+	Route::post('/addTopic/{subject_id}/{group_id}', 'addTopic');
+});
 Route::controller(ProjectController::class)->prefix('/project')->group(function () {
 	Route::get('/getAll', 'getAll');
 	Route::get('/getOne/{id}', 'getOne');
@@ -43,7 +38,31 @@ Route::controller(ProjectController::class)->prefix('/project')->group(function 
 	Route::put('/updateProject/{id}', 'updateProject');
 	Route::delete('/deleteProject', 'deleteProject');
 });
-Route::controller(QuestionController::class)->group(function () {});
-Route::controller(SubjectController::class)->group(function () {});
-Route::controller(TestController::class)->group(function () {});
-Route::controller(TopicController::class)->group(function () {});
+Route::controller(TopicController::class)->prefix('/topic')->group(function () {
+	Route::get('/getMaterials/{topic_id}', 'getMaterials');
+	Route::post('/addLection/{topic_id}', 'addLection');
+	Route::post('/addTest/{topic_id}', 'addTest');
+	Route::put('/updateTest/{test_id}', 'updateTest');
+	Route::put('/updateLection/{lection_id}', 'updateLection');
+	Route::delete('/deleteTest/{test_id}', 'deleteTest');
+	Route::delete('/deleteLection/{lection_id}', 'deleteTest');
+	Route::get('/getMarks/{test_id}', 'getMarks');
+});
+
+Route::controller(AdminController::class)->prefix('/admin')->group(function () {
+	Route::get('/getUsers', 'getUsers');
+	Route::get('/getUser/{id}', 'getUser');
+	Route::get('/getGroups', 'getGroup');
+	Route::get('/getGroup', 'getGroup');
+	Route::get('/getSubjects', 'getSubject');
+	Route::get('/getUsers', 'getUsers');
+	Route::put('/updateUser', 'updateUser');
+	Route::put('/updateGroup', 'updateGroup');
+	Route::put('/updateSubject', 'updateSubject');
+	Route::post('/addUser', 'addUser');
+	Route::post('/addGroup', 'addGroup');
+	Route::post('/addSubject', 'addSubject');
+	Route::delete('/deleteUser', 'deleteUser');
+	Route::delete('/deleteGroup', 'deleteGroup');
+	Route::delete('/deleteSubject', 'deleteSubject');
+});
