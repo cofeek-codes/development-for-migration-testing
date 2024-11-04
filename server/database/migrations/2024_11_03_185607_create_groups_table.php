402bbf22fc63bb2a012469a0bd47b319d8ae6c2f
Renakezo <tchernosckutoff.egor@yandex.ru>
2024-11-04 14:35:35 +0500

    server: add migrations

--- server/database/migrations/2024_11_03_185607_create_groups_table.php
+++ server/database/migrations/2024_11_03_185607_create_groups_table.php
@@ -11,16 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('groups', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('surname');
-            $table->string('patronymic');
-            $table->string('photo');
-            $table->string('password');
-            $table->string('login')->unique();
-            $table->integer('group_id');
-            $table->integer('role_id');
             $table->timestamps();
         });
     }
@@ -30,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('groups');
     }
 };
