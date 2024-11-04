5501dad065b88ca77c659dd339c0cb000fcab99e
Renakezo <tchernosckutoff.egor@yandex.ru>
2024-11-04 14:35:35 +0500

    server: add migrations

--- server/database/migrations/2024_11_03_185656_create_topics_table.php
+++ server/database/migrations/2024_11_03_185656_create_topics_table.php
@@ -11,16 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('topics', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('surname');
-            $table->string('patronymic');
-            $table->string('photo');
-            $table->string('password');
-            $table->string('login')->unique();
-            $table->integer('group_id');
-            $table->integer('role_id');
+            $table->string('title');
+            $table->integer('group_subject_id');
             $table->timestamps();
         });
     }
@@ -30,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('topics');
     }
 };
--- server/database/migrations/2024_11_03_185815_create_marks_table.php
+++ server/database/migrations/2024_11_03_185815_create_marks_table.php
@@ -11,16 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('marks', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('surname');
-            $table->string('patronymic');
-            $table->string('photo');
-            $table->string('password');
-            $table->string('login')->unique();
-            $table->integer('group_id');
-            $table->integer('role_id');
+            $table->string('mark');
+            $table->string('date');
+            $table->integer('test_id');
+            $table->integer('student_id');
+            $table->time('time');
             $table->timestamps();
         });
     }
@@ -30,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('marks');
     }
 };
