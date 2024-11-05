ba9684f1ddc46e7daba9d855383e83ff3bac8d3b
cofeek-codes <kormyshev11@mail.ru>
2024-11-05 16:03:03 +0500

    timer done

--- client/components/topic/Test.tsx
+++ client/components/topic/Test.tsx
@@ -1,7 +1,25 @@
 import React from 'react'
 import Question from './Question'
+import { useTimer } from 'react-timer-hook'
 
 const Test = () => {
+	const expiryTimestamp = new Date()
+	expiryTimestamp.setSeconds(expiryTimestamp.getSeconds() + 600)
+	const {
+		totalSeconds,
+		seconds,
+		minutes,
+		hours,
+		days,
+		isRunning,
+		start,
+		pause,
+		resume,
+		restart,
+	} = useTimer({
+		expiryTimestamp,
+		onExpire: () => console.warn('onExpire called'),
+	})
 	return (
 		<div>
 			{/* title */}
@@ -9,6 +27,10 @@ const Test = () => {
 			{/* question 1 */}
 			<Question />
 			<Question />
+			<div className='absolute left-[25px] bottom-[25px] flex justify-center items-center rounded-[22px] text-[23px] font-bold'>
+				<span>{minutes}:</span>
+				<span>{seconds}</span>
+			</div>
 			<div className='absolute right-[25px] bottom-[25px] bg-lightPurple w-[130px] leading-[15px] h-[45px] flex justify-center items-center rounded-[22px] text-[15px]'>
 				Отправить
 			</div>
