94de32f8c14c5f6391168389125a9f8b5067bbdb
cofeek-codes <kormyshev11@mail.ru>
2024-11-08 00:59:25 +0500

    admin started

---
+++ client/app/admin/page.tsx
@@ -0,0 +1,81 @@
+'use client'
+
+import Image from 'next/image'
+import React, { useState } from 'react'
+
+import arrow from '/assets/arrow.png'
+import Users from '@/components/admin/Users'
+import Groups from '@/components/admin/Groups'
+import Subjects from '@/components/admin/Subjects'
+import News from '@/components/admin/News'
+
+const Admin = () => {
+	// @TODO: add 3rd tab
+	const tabs = [<Users />, <Groups />, <Subjects />, <News />]
+	const [selectedTab, setSelectedTab] = useState<number>(0)
+	return (
+		<div className='h-screen w-[1080px] mx-auto'>
+			<div className='p-[25px]'>
+				{/* title */}
+				<div className='mb-[25px] flex items-center'>
+					<div className='mr-[15px]'>
+						<Image src={arrow} width={26} height={32} alt='go back arrow' />
+					</div>
+					<div className='text-[35px]'>Панель администратора</div>
+				</div>
+				<div className='flex items-center'>
+					{/* left */}
+					<div className='p-[25px] w-[320px] h-[600px] bg-purple rounded-[22px] mr-[25px]'>
+						<div className='*:mb-[2px] last:mb-0'>
+							<div className='hover:bg-buttonsHover rounded-[10px] transition-[0.3s]'>
+								<button
+									className='p-[10px] text-[15px]'
+									onClick={e => {
+										e.preventDefault()
+										setSelectedTab(0)
+									}}
+								>
+									Пользователи
+								</button>
+							</div>
+							<div className='hover:bg-buttonsHover rounded-[10px] transition-[0.3s]'>
+								<button
+									className='p-[10px] text-[15px]'
+									onClick={e => {
+										e.preventDefault()
+										setSelectedTab(1)
+									}}
+								>
+									Группы
+								</button>
+							</div>
+							<div className='hover:bg-buttonsHover rounded-[10px] transition-[0.3s]'>
+								<button className='p-[10px] text-[15px]'>
+									Учебные предметы
+								</button>
+							</div>
+							<div className='hover:bg-buttonsHover rounded-[10px] transition-[0.3s]'>
+								<button
+									onClick={e => {
+										e.preventDefault()
+										setSelectedTab(3)
+									}}
+									className='p-[10px] text-[15px]'
+								>
+									Новости
+								</button>
+							</div>
+						</div>
+					</div>
+
+					{/* right */}
+					<div className='w-[685px] relative p-[25px]  h-[600px] bg-purple rounded-[22px]'>
+						{tabs[selectedTab]}
+					</div>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default Admin
