a74b2feea659054674f1db48f39ecc1ae06689f3
cofeek-codes <kormyshev11@mail.ru>
2024-11-09 15:10:49 +0500

    index redirect

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,8 +1,18 @@
+'use client'
+
+import { useRouter } from 'next/navigation'
+import Cookies from 'js-cookie'
+import { useEffect } from 'react'
+import Loading from '@/components/Loading'
+
 export default function Home() {
-	return (
-		<>
-			<h1 className='font-regular text-[21px]'>Hello font test regular</h1>
-			<h1 className='font-bold text-[21px]'>Hello font test bold</h1>
-		</>
-	)
+	const router = useRouter()
+	useEffect(() => {
+		if (Cookies.get('user_id') == undefined) {
+			router.push('/auth')
+		} else {
+			router.push('/profile')
+		}
+	}, [])
+	return <Loading />
 }
