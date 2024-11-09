29e1e0830db05ee582a0fa95d684465661c90a6f
cofeek-codes <kormyshev11@mail.ru>
2024-11-09 20:50:46 +0500

    student group display fix

--- client/components/profile/Student.tsx
+++ client/components/profile/Student.tsx
@@ -5,6 +5,7 @@ import Link from 'next/link'
 import axiosInstance from '@/utils/axiosInstance'
 import { AxiosError } from 'axios'
 import Loading from '../Loading'
+import { group } from 'console'
 
 const Student = () => {
 	const [data, setData] = useState<any>(null)
@@ -21,16 +22,19 @@ const Student = () => {
 				setError(err)
 			})
 		// getGroup
-		//
-		// axiosInstance
-		// 	.get('/user/getGroups')
-		// 	.then(res => {
-		// 		setData(res.data.message[0])
-		// 		console.log(res.data.message[0])
-		// 	})
-		// 	.catch((err: AxiosError) => {
-		// 		setError(err)
-		// 	})
+
+		axiosInstance
+			.get('/user/getGroup')
+			.then(res => {
+				setData((data: any) => ({
+					...data,
+					groupName: res.data.message[0].name,
+				}))
+				console.log(res.data.message[0])
+			})
+			.catch((err: AxiosError) => {
+				setError(err)
+			})
 	}, [])
 	return (
 		<>
@@ -51,8 +55,8 @@ const Student = () => {
 						{!data && 'Загрузка...'}
 						{data?.name} {data?.surname} {data?.patronymic}
 					</div>
-					{/* @TODO: ask how to get group */}
-					<div className='text-[30px] mb-[30px]'>ДС-301</div>
+					{!data && 'Загрузка...'}
+					<div className='text-[30px] mb-[30px]'>{data?.groupName}</div>
 					<div className='flex transition-[0.3s] hover:bg-buttonsHover h-[75px] justify-center items-center bg-purple w-[228px] rounded-[22px]'>
 						<Link href={'/'} className='w-inherit text-[30px]'>
 							Портфолио
