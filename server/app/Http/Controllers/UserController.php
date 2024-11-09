93d63220fc128f1d9305e780d6d11f9c45ef94d7
cofeek-codes <kormyshev11@mail.ru>
2024-11-09 20:24:55 +0500

    [userController]: getSubjects updated

--- server/app/Http/Controllers/UserController.php
+++ server/app/Http/Controllers/UserController.php
@@ -19,29 +19,20 @@ class UserController extends Controller
         // return ['code' => '200', 'message' => User::find($user_id)->get(['name', 'surname', 'patronymic'])];
     }
 
-    function getGroups(Request $request) {
-        $subjects = Subject::where('user_id', $request->cookie('user_id'))->groupBy('groupId')->get(['group_id']);
-        $groups = [];
-        foreach ($subjects as $subject) {
-            array_push($groups, Group::where('id', $subject->group_id)->get());
-        }
-        return ['code' => '200', 'message' => $groups];
-    }
-
-    function getSubjects($group_id) {
-        return ['code' => '200', 'message' => Subject::where('group_id', $group_id)->get()];
+    function getSubjects(Request $request)
+    {
+        return ['code' => '200', 'message' => Subject::where('group_id', User::find($request->header('user_id'))->group_id)->get()];
     }
 
 
-    function getTopics($subject_id) {
+    function getTopics($subject_id)
+    {
         return ['code' => '200', 'message' => Topic::where('subject_id', '=', $subject_id)->get()];
     }
 
-    function addTopic(Request $request) {
-        return ['code' => '200', 'message' => Topic::create($request->all())];
-    }
 
-    function getNews() {
+    function getNews()
+    {
         return ['code' => 200, 'message' => News::all()];
     }
 }
